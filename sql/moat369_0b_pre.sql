71089986668ed96a5f6d51af96a18c5a1d093e0f
dbarj <rodrigoaraujorge@hotmail.com>
2018-09-27 10:05:05 -0300

    Update realease version
    Update realease version

--- sql/moat369_0b_pre.sql
+++ sql/moat369_0b_pre.sql
@@ -6,8 +6,8 @@ SET FEED OFF
 SET ECHO OFF
 SET TIM OFF
 SET TIMI OFF
-DEF moat369_fw_vYYNN = 'v1806'
-DEF moat369_fw_vrsn  = '&&moat369_fw_vYYNN. (2018-06-14)'
+DEF moat369_fw_vYYNN = 'v1807'
+DEF moat369_fw_vrsn  = '&&moat369_fw_vYYNN. (2018-09-27)'
 
 -- Define all functions and files:
 @@moat369_fc_define_files.sql
