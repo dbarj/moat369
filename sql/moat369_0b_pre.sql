060b02113d1af17a7797cf0b751974b711209272
Rodrigo Jorge <rodrigoaraujorge@hotmail.com>
2021-02-10 17:56:36 -0300

    Final prepare

--- sql/moat369_0b_pre.sql
+++ sql/moat369_0b_pre.sql
@@ -6,8 +6,8 @@ SET FEED OFF
 SET ECHO OFF
 SET TIM OFF
 SET TIMI OFF
-DEF moat369_fw_vYYNN = 'v2001'
-DEF moat369_fw_vrsn  = '&&moat369_fw_vYYNN. (2020-01-09)'
+DEF moat369_fw_vYYNN = 'v21.01'
+DEF moat369_fw_vrsn  = '&&moat369_fw_vYYNN. (2021-02-10)'
 
 -- Define all functions and files:
 @@moat369_fc_define_files.sql
